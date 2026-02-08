96db9ea96057476ce47c323aa89c8bc58539788e
Legends <966368+Legends@users.noreply.github.com>
2026-02-09 00:14:39 +0100

    Split flyouts into separate views

---
+++ TOTP/Views/SettingsFlyoutView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace TOTP.Views
+{
+    public partial class SettingsFlyoutView : UserControl
+    {
+        public SettingsFlyoutView()
+        {
+            InitializeComponent();
+        }
+    }
+}
