a45ec6ee768cee0c9946f4e8aadb0dbc5c6c69fa
Legends <966368+Legends@users.noreply.github.com>
2026-02-16 21:05:11 +0100

    Ensure MainWindow initializes ViewModel on load

--- TOTP/Views/MainWindow.xaml.cs
+++ TOTP/Views/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Syncfusion.SfSkinManager;
 using Syncfusion.Windows.Shared;
 using System;
+using System.Windows;
 using TOTP.Resources;
 using TOTP.Services.Interfaces;
 
@@ -8,13 +9,28 @@ namespace TOTP.Views;
 
 public partial class MainWindow : ChromelessWindow
 {
+    private readonly IMainViewModel _vm;
+    private bool _isInitialized;
+
     public MainWindow(IMainViewModel vm)
     {
         InitializeComponent();
 
-        DataContext = vm ?? throw new ArgumentNullException(nameof(vm));
+        _vm = vm ?? throw new ArgumentNullException(nameof(vm));
+        DataContext = _vm;
         Title = UI.ui_Window_Title_TOTP_Manager;
 
         SkinManagerHelper.SetScrollBarMode(this, ScrollBarMode.Default);
+
+        Loaded += OnLoadedAsync;
+    }
+
+    private async void OnLoadedAsync(object sender, RoutedEventArgs e)
+    {
+        if (_isInitialized)
+            return;
+
+        _isInitialized = true;
+        await _vm.InitializeAsync();
     }
 }
