a17a4c8b1e0ccbc0b878f5c37aaef462144e8d6a
Legends <legends@users.noreply.github.com>
2026-02-22 10:13:18 +0100

    RESTRUCTURE

--- TOTP/UserControls/FlyoutHost.xaml.cs
+++ TOTP/UserControls/FlyoutHost.xaml.cs
@@ -9,10 +9,8 @@ namespace TOTP.UserControls;
 
 public partial class FlyoutHost : UserControl
 {
-    public FlyoutHost()
-    {
-        InitializeComponent();
-    }
+
+    #region ### PROPERTIES ####
 
     public static readonly DependencyProperty IsOpenProperty =
         DependencyProperty.Register(
@@ -21,69 +19,9 @@ public partial class FlyoutHost : UserControl
             typeof(FlyoutHost),
             new FrameworkPropertyMetadata(false));
 
-    public static readonly DependencyProperty WarmUpOnLoadProperty =
-        DependencyProperty.Register(
-            nameof(WarmUpOnLoad),
-            typeof(bool),
-            typeof(FlyoutHost),
-            new FrameworkPropertyMetadata(false, OnWarmUpOnLoadChanged));
-
-    public bool IsOpen
-    {
-        get => (bool)GetValue(IsOpenProperty);
-        set => SetValue(IsOpenProperty, value);
-    }
-
-    public bool WarmUpOnLoad
-    {
-        get => (bool)GetValue(WarmUpOnLoadProperty);
-        set => SetValue(WarmUpOnLoadProperty, value);
-    }
-
-    private static void OnWarmUpOnLoadChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-    {
-        if (d is not FlyoutHost host || e.NewValue is not bool shouldWarmUp || !shouldWarmUp)
-            return;
-
-        host.Loaded -= host.OnWarmUpLoaded;
-        host.Loaded += host.OnWarmUpLoaded;
-    }
-
-    private void OnWarmUpLoaded(object sender, RoutedEventArgs e)
-    {
-        Loaded -= OnWarmUpLoaded;
-
-        if (IsOpen)
-            return;
-
-        if (FlyoutContentPresenter is null || FlyoutPanel is null || HostRoot is null)
-            return;
-
-        var previousVisibility = HostRoot.Visibility;
-        var previousOpacity = HostRoot.Opacity;
-        var previousHitTest = HostRoot.IsHitTestVisible;
-
-        // HostRoot.Visibility = Visible wont work, you would override the binding to IsOpen
-        // then after onwarmup the fylout wont show!
-        // All props with bindings have to be set like this:
-        HostRoot.SetCurrentValue(VisibilityProperty, Visibility.Visible);
-        HostRoot.SetCurrentValue(OpacityProperty, 0d);
-        HostRoot.SetCurrentValue(IsHitTestVisibleProperty, false);
-
-        FlyoutContentPresenter.ApplyTemplate();
-        FlyoutContentPresenter.UpdateLayout();
-        FlyoutPanel.ApplyTemplate();
-        FlyoutPanel.UpdateLayout();
-        HostRoot.UpdateLayout();
-
-        HostRoot.SetCurrentValue(OpacityProperty, previousOpacity);
-        HostRoot.SetCurrentValue(IsHitTestVisibleProperty, previousHitTest);
-        HostRoot.SetCurrentValue(VisibilityProperty, previousVisibility);
-    }
-
     public static readonly DependencyProperty FlyoutContentProperty =
-        DependencyProperty.Register(nameof(FlyoutContent), typeof(object), typeof(FlyoutHost),
-            new FrameworkPropertyMetadata(null));
+    DependencyProperty.Register(nameof(FlyoutContent), typeof(object), typeof(FlyoutHost),
+        new FrameworkPropertyMetadata(null));
 
     public object? FlyoutContent
     {
@@ -180,4 +118,73 @@ public partial class FlyoutHost : UserControl
         get => (int)GetValue(PanelZIndexProperty);
         set => SetValue(PanelZIndexProperty, value);
     }
+
+    public static readonly DependencyProperty WarmUpOnLoadProperty =
+        DependencyProperty.Register(
+            nameof(WarmUpOnLoad),
+            typeof(bool),
+            typeof(FlyoutHost),
+            new FrameworkPropertyMetadata(false, OnWarmUpOnLoadChanged));
+
+    public bool IsOpen
+    {
+        get => (bool)GetValue(IsOpenProperty);
+        set => SetValue(IsOpenProperty, value);
+    }
+
+    public bool WarmUpOnLoad
+    {
+        get => (bool)GetValue(WarmUpOnLoadProperty);
+        set => SetValue(WarmUpOnLoadProperty, value);
+    }
+
+    #endregion
+
+    public FlyoutHost()
+    {
+        InitializeComponent();
+    }
+
+    private static void OnWarmUpOnLoadChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not FlyoutHost host || e.NewValue is not bool shouldWarmUp || !shouldWarmUp)
+            return;
+
+        host.Loaded -= host.OnWarmUpLoaded;
+        host.Loaded += host.OnWarmUpLoaded;
+    }
+
+    private void OnWarmUpLoaded(object sender, RoutedEventArgs e)
+    {
+        Loaded -= OnWarmUpLoaded;
+
+        if (IsOpen)
+            return;
+
+        if (FlyoutContentPresenter is null || FlyoutPanel is null || HostRoot is null)
+            return;
+
+        var previousVisibility = HostRoot.Visibility;
+        var previousOpacity = HostRoot.Opacity;
+        var previousHitTest = HostRoot.IsHitTestVisible;
+
+        // HostRoot.Visibility = Visible wont work, you would override the binding to IsOpen
+        // then after onwarmup the fylout wont show!
+        // All props with bindings have to be set like this:
+        HostRoot.SetCurrentValue(VisibilityProperty, Visibility.Visible);
+        HostRoot.SetCurrentValue(OpacityProperty, 0d);
+        HostRoot.SetCurrentValue(IsHitTestVisibleProperty, false);
+
+        FlyoutContentPresenter.ApplyTemplate();
+        FlyoutContentPresenter.UpdateLayout();
+        FlyoutPanel.ApplyTemplate();
+        FlyoutPanel.UpdateLayout();
+        HostRoot.UpdateLayout();
+
+        HostRoot.SetCurrentValue(OpacityProperty, previousOpacity);
+        HostRoot.SetCurrentValue(IsHitTestVisibleProperty, previousHitTest);
+        HostRoot.SetCurrentValue(VisibilityProperty, previousVisibility);
+    }
+
+
 }
